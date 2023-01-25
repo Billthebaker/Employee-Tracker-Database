9a3811abfc3180bc0170abf4dc24d8d609ba9d08
unknown <dbp1795@aim.com>
2023-01-25 18:10:29 -0500

    filled part of the schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,4 +5,27 @@ USE employees_db;
 
 CREATE TABLE depts (
     id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE role (
+     id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id)
+
 )
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL AUTO_INCREMENT,
+    manager_id INT,
+    PRIMARY KEY(id)
+)
+
+
